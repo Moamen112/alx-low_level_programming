ae18a16b8e65026d593a9c3b9f8e2515a6af2cad
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-08 16:05:40 -0800

    printing to stderr using fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing ouput followed by a new line to the standard error without printf and puts
+ * return 0 (Success)
+ */
+
+int main(void) 
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 2);
+		return (0);
+}
