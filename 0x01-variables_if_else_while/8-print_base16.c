3940038b0b3327062a52981c54e6106e8023eec0
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-10 02:37:55 +0200

    Print from 0 to 16 in hexa usint putchar in C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print from 0 to 16 in hexadecimal
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+
+	x = 'a';
+	while (x <= 'f')
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}
