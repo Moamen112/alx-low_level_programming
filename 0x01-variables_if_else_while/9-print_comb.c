26a1ae63cb018332ab3f6392cf1279b78c5e2c4c
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-10 02:44:34 +0200

    Prints all combination of a single digit in C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - prints all combination of a single digit
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
