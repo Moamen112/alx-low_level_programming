e8ba3ca05872878e96fbf4914ee3f2d003c9a6c4
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-13 21:16:16 +0200

    Check the sign of the number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - Print The sign of number
+ * @n: The number to be tested
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
