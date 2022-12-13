e56cd48a4bbbb65b1c7c93c25bbc0ab168345b15
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-14 00:43:16 +0200

    Printing hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int m, h = 0;
+
+	while (h <= 12)
+	{
+		while (m <= 59)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+
+			if (h == 12 && m == 59)
+				break;
+	
+			
+			_putchar('\n');
+		}
+	}
+}
