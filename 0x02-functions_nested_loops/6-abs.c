4decf382310af29634f51f5b0b9216269de80337
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-13 21:26:43 +0200

    Printing the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Print the absolute value of a number
+ * @num: the number to be tested
+ * Return: The absolute value
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+		i *= -1;
+	return i 
+}
