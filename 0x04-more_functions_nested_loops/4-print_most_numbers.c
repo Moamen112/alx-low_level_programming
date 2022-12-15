0143b0abb82c209b40ab68dca0284780c2606281
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-15 12:50:32 +0200

    Printing most of the numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print most of the numbers
+ *
+ * Return: Nothing.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + '0');
+	}
+}
