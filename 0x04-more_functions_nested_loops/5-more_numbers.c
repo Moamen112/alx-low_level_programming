8f7ea04cbdb2943eca9f263459019511900522e0
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-15 13:03:45 +0200

    Fixing output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			else
+				_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
