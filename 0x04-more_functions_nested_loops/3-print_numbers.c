cd7362d4c5f5dd57130a09c45ab575c588000016
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-15 12:39:34 +0200

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Printing numbers from 0 to 10
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
