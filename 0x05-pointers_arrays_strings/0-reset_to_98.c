3c1b8cb642ea3a1581b77e4ee896f958e71fed92
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-20 03:34:01 +0200

    Return value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update a variable value
+ * @*n: the pointer
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+None Empty Readme
