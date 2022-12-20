37a0e3a7977a83e5c3d0f039f5524c27e452028b
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-20 04:11:45 +0200

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+ #include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: the strong
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char str[10000];
+
+	i = j = 0;
+
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+
+	s[j++] = '\0';
+}
