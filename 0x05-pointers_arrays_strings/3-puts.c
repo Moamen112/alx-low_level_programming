ccbbf65a3cd8610b15c14c32863aa53b1cabad6b
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-20 03:48:09 +0200

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: The string
+ * Return: Nothing
+ */
+
+void _puts(char *str);
+{
+	while (*str)
+	{
+		_putchar(*str++)
+	}
+}
