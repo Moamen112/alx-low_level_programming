e8c20c9051d78e1f5814f5e5ce4c4a85a5ba306e
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-23 02:17:51 +0200

    encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into leetspeek.
+ * @s: pointer to input string.
+ * Return: Returns pointer to leetspeek string.
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char arr[] = "aAeEoOtTlL";
+	char let[] = "43071";
+
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; let[j] != '\0'; j++)
+		{
+			if (s[i] == arr[j])
+			{
+				s[i] = let[j / 2];
+			}
+		}
+	}
+}
