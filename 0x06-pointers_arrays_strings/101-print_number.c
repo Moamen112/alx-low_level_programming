e1a285028b68e60e012ac31691dab0d7e610a38a
Moamen Shehab <memo.shehab30@gmail.com>
2022-12-23 04:31:54 +0200

    prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: Input number
+ */
+
+void print_number(int n)
+{
+	int temp, pow, result;
+
+	pow = 1;
+
+	if (n >= 0)
+		result = n * -1;
+	else
+	{
+		result = n;
+		_putchar('_');
+	}
+
+	temp = result;
+	while (temp <= -10)
+	{
+		pow *= 10;
+		temp /= 10;
+	}
+
+	while (pow >= 1)
+	{
+		_putchar(((result / pow) % 10) * -1 + '0');
+		pow /= 10;
+	}
+}
