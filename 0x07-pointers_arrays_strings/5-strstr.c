c1fd557464aa7c29504071cd89d0a6e96b536b9d
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-03 04:34:58 +0200

    Fixing errors

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,7 +22,7 @@ char *_strstr(char *haystack, char *needle)
 			do {
 				if (needle[i + 1] == '\0')
 					return (haystack);
-				i++;	
+				i++;
 			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,11 +11,11 @@ void print_diagsums(int *a, int size)
 	int i, x, y;
 
 	x = y = 0;
-	for (i = 0; i < (size * size); i += size - 1)
+	for (i = 0; i < (size * size); i += size + 1)
 	{
 		x += a[i];
 	}
-	
+
 	for (i = size - 1; i < (size * size - 1); i += size - 1)
 		y += a[i];
 	printf("%d, %d\n", x, y);
