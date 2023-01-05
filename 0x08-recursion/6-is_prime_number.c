239c775fff34222c1fd5adb65551f01691e9d0f8
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-05 05:05:26 +0200

    Checking the prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * is_prime - checks recursively the input from is_prime_number
+  * @n: iterator
+  * @base: base number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+
+int is_prime(int n, int base)
+{
+	if (base % n == 0 || base < 2)
+		return (0);
+	else if (n == base - 1)
+		return (1);
+	else if (base > n)
+		return (is_prime(n + 1), base);
+	return (1);
+}
+
+/**
+  * is_prime_number - checks if the number is a prime number
+  * @n: the number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+
+int is_prime_number(int n)
+{
+	return (is_prime(2, n));
+}
