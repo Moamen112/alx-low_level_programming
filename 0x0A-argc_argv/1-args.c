1f59e7687f343b9891fd04c9251b2cdf07c55d40
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-07 00:39:13 +0200

    Argument num

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints out all of the arguments passed to it.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	
+	return (0);
+}
