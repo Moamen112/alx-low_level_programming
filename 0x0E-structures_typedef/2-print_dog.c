5d4ebb54d336d6059d90212ea914ebfc3d7f196e
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-17 01:11:51 +0200

    Fixing error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age)
+		printf("Age: %f\n", d->age);
 		d->owner ? printf("Owner: %s\n", d->owner) : printf("nil\n");
 	}
 }
