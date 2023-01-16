4d80c715fde765784cb649075ee0212c841015ce
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-17 01:27:04 +0200

    Fixing error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,35 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int name_len, owner_len, i;
-	dog_t *dog;
+	dog_t *doggy;
 
 	name_len = owner_len = 0;
 	while (name[name_len++])
 		;
 	while (owner[owner_len++])
 		;
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
 
-	dog->name = malloc(name_len * sizeof(dog->name));
-	if (dog->name == NULL)
+	doggy->name = malloc(name_len * sizeof(doggy->name));
+	if (doggy->name == NULL)
 	{
-		free(dog);
+		free(doggy);
 		return (NULL);
 	}
 	for (i = 0; i < name_len; i++)
-		dog->name[i] = name[i];
+		doggy->name[i] = name[i];
 
-	dog->age = age;
+	doggy->age = age;
 
-	dog->owner = malloc(owner_len * sizeof(dog->owner));
-	if (dog->owner == NULL)
+	doggy->owner = malloc(owner_len * sizeof(doggy->owner));
+	if (doggy->owner == NULL)
 	{
-		free(dog);
+		free(doggy);
 		return (NULL);
 	}
 	for (i = 0; i < owner_len; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
