b8927044dd2523202a5a0d576cfd2493c747eea2
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-16 23:41:33 +0200

    Printing file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file
+ * @void: Accept nothing
+ * Return: Always (0) on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
