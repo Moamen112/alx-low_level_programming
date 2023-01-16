06bed4980e4f5b4d5ea3b738a29afdb6778a9eb8
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-17 00:51:44 +0200

    Fixing error

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -9,8 +9,8 @@
 int main(void)
 {
     struct dog my_dog;
-
-    my_dog.name = "Poppy";
+	
+	my_dog.name = NULL;
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
     print_dog(&my_dog);
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)");
-		d->age > 0 ? printf("Age: %f\n", d->age) : printf("Age: (nil)");
-		d->owner ? printf("Owner: %s\n", d->name) : printf("Owner: (nil)");
+		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+		d->age > 0 ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+		d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }
