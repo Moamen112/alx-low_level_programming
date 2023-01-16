bc9d73c232a4c825e28a2ba064d13796311dc5f8
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-17 00:09:49 +0200

    Adding dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
 #ifndef DOH_H
 #define DOG_H
 
+/**
+ * struct dog - Define data for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+int _putchar(char c);
 
 #endif
