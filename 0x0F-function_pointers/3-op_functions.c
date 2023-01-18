5873e1c06b6a13e284056db1f309d9d18a39d031
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-19 00:56:20 +0200

    Simple calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ *op_add - Adds two numbers
+ *@a:the first number
+ *@b:the second number
+ *Return:The sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - substract two numbers
+ *@a:the first number
+ *@b:the second number
+ *Return:The substraction of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - Multiply two numbers
+ *@a:the first number
+ *@b:the second number
+ *Return:The Multiplication of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - division two numbers
+ *@a:the first number
+ *@b:the second number
+ *Return:The division of a and b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - module two numbers
+ *@a:the first number
+ *@b:the second number
+ *Return:The reminder of division of a and b
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
