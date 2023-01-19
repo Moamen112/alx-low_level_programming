6ec16f9d0052268e7eaaa4e235afe6fd0434dbf2
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-20 01:27:28 +0200

    Fixing erro

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,14 @@
-#ifndef VARIADIC_FUN
-#define VARIADIC_FUN
+#ifndef VARIADIC_FUN_H
+#define VARIADIC_FUN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct valid
+{
+	char *ptr;
+	void (*ptr)(va_list arg);
+} valid_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
