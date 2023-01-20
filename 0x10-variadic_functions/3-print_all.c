2788fa25eaba82db7de50dc1d3d1f47675d32f20
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-20 02:43:43 +0200

    Fixing error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,39 +14,40 @@ void print_all(const char * const format, ...)
 	va_list ap;
 
 	va_start(ap, format);
-	while (format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
-				flag = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(ap, int));
-				flag = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				flag = 1;
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-				if (!s)
-					s = "(nil)";
-				printf("%s", s);
-				flag = 1;
-				break;
-			default:
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(ap, int));
+					flag = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(ap, int));
+					flag = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(ap, double));
+					flag = 1;
+					break;
+				case 's':
+					s = va_arg(ap, char *);
+					printf("%s", s ? s : "(nil)");
+					flag = 1;
+					break;
+				default:
+					break;
+			}
+			if (flag && format[i + 1])
+			{
+				printf(", ");
+				flag = 0;
+			}
+			i++;
 		}
-		if (flag && format[i + 1])
-		{
-			printf(", ");
-			flag = 0;
-		}
-		i++;
 	}
-	va_end(ap);
-	printf("\n");
+		va_end(ap);
+		printf("\n");
 }
