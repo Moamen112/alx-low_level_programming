f59568c25fa443da49fd2a73a1fcb0a4c99c047f
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-26 22:19:58 +0200

    Printing text before main runs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void) __attribute__((constructor));
+
+/**
+* first - prints a message before main is executed
+*
+* Return: void
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
