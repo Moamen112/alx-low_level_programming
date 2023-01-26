8fd3a8ef86ec9aa1fb48eb90362348b22a35595a
Moamen Shehab <memo.shehab30@gmail.com>
2023-01-26 22:13:17 +0200

    Fixing betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,10 +4,7 @@
  * free_list - frees a list_t list
  * @head: pointer to head of list
  *
- * This function takes in the head of a list_t list as a parameter. 
- * It uses a temporary pointer to iterate through the list, freeing the memory 
- * allocated for each node's str and the node itself, before moving on to the next node. 
- * Once all the nodes have been freed, the list is completely deallocated and the memory is returned to the system.
+ * Return: Nothing
  */
 
 void free_list(list_t *head)
