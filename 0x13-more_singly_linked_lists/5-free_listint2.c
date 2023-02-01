d7691f28971b8c10417eefddf49611cfcffad5ae
Moamen Shehab <memo.shehab30@gmail.com>
2023-02-01 03:49:04 +0200

    Fixing Error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
-    }
+	}
 	head = (NULL);
 }
