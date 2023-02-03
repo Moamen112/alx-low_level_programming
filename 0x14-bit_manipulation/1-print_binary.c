7936b311d96adf6921f624b57518b08f9d2ce921
Moamen Shehab <memo.shehab30@gmail.com>
2023-02-03 04:14:45 +0200

    Fixing error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,8 +14,7 @@ void print_binary(unsigned long int n)
 
 	for (tmp = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		if ((n >> 1) & 1)
-			tmp = 1;
+		tmp = 1;
 		if (tmp == 1)
 			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
