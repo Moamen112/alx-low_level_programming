75ff7649543b2e5d204cef39182d1d8e85ae0fd3
Moamen Shehab <memo.shehab30@gmail.com>
2023-02-03 04:17:52 +0200

    Fixing error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,14 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to be converted to binary
+ *
+ * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	int i, tmp;
-
-	if (n == 0)
-		_putchar('0');
-
-	for (tmp = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
-	{
-		tmp = 1;
-		if (tmp == 1)
-			((n >> i) & 1) ? _putchar('1') : _putchar('0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
