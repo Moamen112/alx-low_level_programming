317538cabf74a723128862e235c7721e3eb291a7
Moamen Shehab <memo.shehab30@gmail.com>
2023-02-03 04:54:08 +0200

    Fixing error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		result = (result << 1) + (b[i] - '0');
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
